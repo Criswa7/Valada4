fb31d6a8f4748c7e9e4cfc8abde3ca8739b76cef
Cristian Florez <133178936+Criswa7@users.noreply.github.com>
2024-07-12 16:30:36 +0000

    vote system with images

--- src/components/Voting.js
+++ src/components/Voting.js
@@ -3,13 +3,22 @@ import './Voting.css';
 
 function Voting() {
   const [fights, setFights] = useState([
-    { id: 1, fighter1: 'Agustin51', fighter2: 'Carrera', votes: { fighter1: 0, fighter2: 0 } },
-    { id: 2, fighter1: 'Guanyar', fighter2: 'La Cobra', votes: { fighter1: 0, fighter2: 0 } },
-    { id: 3, fighter1: 'Zeling y Nissaxter', fighter2: 'Amablitz y Alana', votes: { fighter1: 0, fighter2: 0 } },
-    { id: 4, fighter1: 'Viruzz', fighter2: 'Shelao', votes: { fighter1: 0, fighter2: 0 } },
-    { id: 5, fighter1: 'Rey de la pista', fighter2: '10 boxeadores', votes: { fighter1: 0, fighter2: 0 }, 
-      description: 'Roberto Cein - Peldanyos - Aldo Geo - Unicornio - Folagor - Skain - Karchez - Sezar Blue - Peliganger - Will' },
-    { id: 6, fighter1: 'YoSoyPlex', fighter2: 'El Mariana', votes: { fighter1: 0, fighter2: 0 } },
+    { id: 1, fighter1: 'Agustin51', fighter2: 'Carreraaa', votes: { fighter1: 0, fighter2: 0 }, size: 'big' },
+    { id: 2, fighter1: 'Guanyar', fighter2: 'La Cobra', votes: { fighter1: 0, fighter2: 0 }, size: 'big' },
+    { id: 3, fighter1: 'Zeling y Nissaxter', fighter2: 'Ama Blitz y Alana', votes: { fighter1: 0, fighter2: 0 }, size: 'small' },
+    { id: 4, fighter1: 'Viruzz', fighter2: 'Shelao', votes: { fighter1: 0, fighter2: 0 }, size: 'big' },
+    { 
+      id: 5, 
+      type: 'multiple',
+      title: 'Rey de la pista',
+      fighters: [
+        'Roberto Cein', 'Peldanyos', 'Aldo Geo', 'Unicornio', 'Folagor',
+        'Skain', 'Karchez', 'Sezar Blue', 'Pelicanger', 'Will'
+      ],
+      votes: {},
+      size: 'small'
+    },
+    { id: 6, fighter1: 'Plex', fighter2: 'El Mariana', votes: { fighter1: 0, fighter2: 0 }, size: 'big' },
   ]);
 
   const [userVotes, setUserVotes] = useState({});
@@ -33,8 +42,13 @@ function Voting() {
 
     const updatedFights = fights.map(fight => {
       if (fight.id === fightId) {
-        const updatedVotes = { ...fight.votes, [fighter]: fight.votes[fighter] + 1 };
-        return { ...fight, votes: updatedVotes };
+        if (fight.type === 'multiple') {
+          const updatedVotes = { ...fight.votes, [fighter]: (fight.votes[fighter] || 0) + 1 };
+          return { ...fight, votes: updatedVotes };
+        } else {
+          const updatedVotes = { ...fight.votes, [fighter]: fight.votes[fighter] + 1 };
+          return { ...fight, votes: updatedVotes };
+        }
       }
       return fight;
     });
@@ -47,30 +61,45 @@ function Voting() {
     localStorage.setItem('userVotes', JSON.stringify(updatedUserVotes));
   };
 
+  const getImageFileName = (fighter, size) => {
+    const baseName = fighter.toLowerCase().replace(' y ', '-').replace(' ', '-');
+    return `${baseName}-${size}.webp`;
+  };
+
+  const renderFighterButton = (fightId, fighter, votes, size) => (
+    <button 
+      onClick={() => handleVote(fightId, fighter)}
+      disabled={userVotes[fightId]}
+      className={`${userVotes[fightId] === fighter ? 'voted' : ''} ${size}`}
+    >
+      <img src={`/images/${getImageFileName(fighter, size)}`} alt={fighter} />
+      <span>{fighter}</span>
+      <span>({votes} votos)</span>
+    </button>
+  );
+
   return (
     <div className="voting">
       <h2>Votaciones para los Combates</h2>
       {fights.map(fight => (
         <div key={fight.id} className="fight">
           <h3>Combate {fight.id}</h3>
-          <div className="fighters">
-            <button 
-              onClick={() => handleVote(fight.id, 'fighter1')}
-              disabled={userVotes[fight.id]}
-              className={userVotes[fight.id] === 'fighter1' ? 'voted' : ''}
-            >
-              {fight.fighter1} ({fight.votes.fighter1} votos)
-            </button>
-            <span>VS</span>
-            <button 
-              onClick={() => handleVote(fight.id, 'fighter2')}
-              disabled={userVotes[fight.id]}
-              className={userVotes[fight.id] === 'fighter2' ? 'voted' : ''}
-            >
-              {fight.fighter2} ({fight.votes.fighter2} votos)
-            </button>
-          </div>
-          {fight.description && <p className="fight-description">{fight.description}</p>}
+          {fight.type === 'multiple' ? (
+            <div className="multiple-fighters">
+              <h4>{fight.title}</h4>
+              {fight.fighters.map(fighter => (
+                <div key={fighter} className="fighter-button">
+                  {renderFighterButton(fight.id, fighter, fight.votes[fighter] || 0, fight.size)}
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div className="fighters">
+              {renderFighterButton(fight.id, fight.fighter1, fight.votes.fighter1, fight.size)}
+              <span>VS</span>
+              {renderFighterButton(fight.id, fight.fighter2, fight.votes.fighter2, fight.size)}
+            </div>
+          )}
         </div>
       ))}
     </div>
